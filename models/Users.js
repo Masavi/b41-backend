b4df3caacb9e3194f3a6b77dc137a2c8a098aa82
maui <maui@devf.mx>
2020-06-08 21:08:10 -0500

    Instalar bcrypt y agregar encriptado automatico de contraseñas

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const UsersSchema = mongoose.Schema({
   first_name: {
@@ -25,6 +28,30 @@ const UsersSchema = mongoose.Schema({
     type: Boolean,
     default: true,
   },
+});
+
+// eslint-disable-next-line func-names
+UsersSchema.pre('save', function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  // eslint-disable-next-line consistent-return
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (errHash, hash) => {
+      if (errHash) return next(errHash);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      return next();
+    });
+  });
+  return false;
 });
 
 const Users = mongoose.model('Users', UsersSchema);
