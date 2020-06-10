7834d254e70257dd200913d7145ad7bfdc15a001
maui <maui@devf.mx>
2020-06-09 22:11:14 -0500

    Crear middleware de verificacion de token para proteger rutas

---
+++ middlewares/index.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  verifyToken: (req, res, next) => {
+    /*
+        el objeto req.headers contiene adentro el atributo "authorization" y viene así:
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZTAzNjIzMTJiN2FlNzM0YTkxMWNmYSIsImVtYWlsIjoiZmVybmFuZG9AZ21haWwuY29tIiwiZmlyc3RfbmFtZSI6IkZlcm5hbmRvIiwiaWF0IjoxNTkxNzU2NDExfQ.QJkiOKOhZmyTq8m6lh8OpfYnGZLceqJtjr3YQoTcfVM'
+    */
+    try {
+      const { authorization } = req.headers;
+      // authorization.split(' '); // -> [ "Bearer", "eyJhbG..." ]
+      const token = authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('⛽️', decoded);
+      req.decoded = decoded;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Auth error', error });
+    }
+  },
+};
