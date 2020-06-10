f6139204d1b63bacc255296e173d456a2114058f
maui <maui@devf.mx>
2020-06-09 21:36:17 -0500

    Agregar utilidad de creación de JWT

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,22 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   comparePasswords: (loggedPassword, registerdPassword) => bcrypt
     .compare(loggedPassword, registerdPassword),
+  createToken: (user) => {
+    const payload = {
+      // eslint-disable-next-line no-underscore-dangle
+      id: user._id,
+      email: user.email,
+      first_name: user.first_name,
+    };
+
+    try {
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+      return token;
+    } catch (error) {
+      return undefined;
+    }
+  },
 };
