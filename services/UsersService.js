ab9ebcea908d48ef0871cc747bda21e7cf3df1ee
maui <maui@devf.mx>
2020-06-11 20:08:17 -0500

    Agregar ejemplos de cómo manejar servicios de documentos embebidos (actualizar y eliminar)

--- services/UsersService.js
+++ services/UsersService.js
@@ -12,4 +12,27 @@ module.exports = {
     user.posts.push(post);
     return user.save();
   },
+  removePost: (user, idPost) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const filteredPosts = user.posts.filter((post) => !post._id === idPost);
+    // eslint-disable-next-line no-param-reassign
+    user.posts = filteredPosts;
+    return user.save();
+  },
+  updatePost: (user, updatedPost) => {
+    // eslint-disable-next-line no-underscore-dangle
+    // const foundPost = user.posts.find((post) => post._id === updatedPost._id);
+    // La forma de arriba y la forma de abajo ambos nos devuelven lo mismo
+    // eslint-disable-next-line no-underscore-dangle
+    const foundPost = user.posts.id(updatedPost._id);
+    if (!foundPost) return undefined;
+    const updatedPosts = user.posts.map((post) => {
+      // eslint-disable-next-line no-underscore-dangle
+      if (post._id === foundPost._id) return foundPost;
+      return post;
+    });
+    // eslint-disable-next-line no-param-reassign
+    user.posts = updatedPosts;
+    return user.save();
+  },
 };
